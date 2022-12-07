e369bce87d8650883860d64697e4cb1ed806c64b
Viacheslav Kuminov <kuminov200220@gmail.com>
2022-12-07 19:11:01 +0700

    выполнил одно усложненное пятое усложненное задание

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 "use strict";
 
-let strTrans;
+const arr = [204, 242, 415, 584, 124, 711, 777];
 
-const stringReduction = function (str) {
-
-    if (typeof (str) !== "string") {
-        return (TypeError('Введена не строка'));
+for (let i = 0; i < arr.length; i++) {
+    let chr = arr[i].toString()[0];
+    if (chr === '2' || chr === '4') {
+        console.log(arr[i]);
     }
-    strTrans = str.trim();
-    strTrans.length > 30 ? strTrans = str.trim().substring(0, 29) + "..." : strTrans = str.trim(); 
-    return strTrans;
-};
+}
 
-console.log(stringReduction(prompt('Введите какую-нибудь строку')));
+for (let i = 1; i < 101; i++) {
+    for (let j = 2; j <= i; j++) {
+        if ((i % j === 0) && (j < i)) {
+            break;
+        } else if (j === i) {
+            console.log('Число ' + i + ', делители этого числа: 1 и ' + i);
+            break;
+        }
+    }
+}

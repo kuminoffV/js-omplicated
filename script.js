ad837cacc638b4cde60bb654940f7327f031cf2f
Viacheslav Kuminov <kuminov200220@gmail.com>
2022-12-10 00:56:39 +0700

    исправил ошибки в седбмом усложненном задании

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-"use strict";
+'use strict';
 
-const week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+const week = [
+'Понедельник',
+'Вторник',
+'Среда',
+'Четверг',
+'Пятница',
+'Суббота',
+'Воскресенье',
+];
 let day = new Date().getDay();
 let today = week[day];
+console.log(`today ${today}`);
 
 const weekList = function (arr) {
-    for (let key of arr) {
-
-        if (key === 'Суббота' || key === 'Воскресенье') {
-            document.writeln(`<p><i>${key}</i></p>`);
-        } else if (key === today) {
-            document.writeln(`<p><b>${key}</b></p>`);
-        } else {
-            document.writeln(`<p>${key}</p>`);
-        }
-    }
-}
+     for (let key of arr) {
+          switch (true) {
+               case (key === "Суббота" || key === "Воскресенье") && key === today:
+                    document.writeln(`<p><i><b>${key}</b></i></p>`);
+                    break;
+               case key === "Суббота" || key === "Воскресенье":
+                    document.writeln(`<p><i>${key}</i></p>`);
+                    break;
+               case key === today:
+                    document.writeln(`<p><b>${key}</b></p>`);
+                    break;
+               default:
+                    document.writeln(`<p>${key}</p>`);
+                    break;
+          }
+     }
+};
 
 weekList(week);

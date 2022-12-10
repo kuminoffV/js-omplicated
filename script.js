efde95c74a4598f0cd84186b62bb196d54c20d73
Viacheslav Kuminov <kuminov200220@gmail.com>
2022-12-10 23:14:14 +0700

    седьмое усложненное задание

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
 'use strict'; 
 
-const week = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье',];
-let day = new Date().getDay() - 1;
-let today = week[day];
-console.log(`today ${today}`);
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const today = (6 + new Date().getDay()) % 7;
 
-const weekList = function (arr) {
-     for (let key of arr) {
-          switch (true) {
-               case (key === "Суббота" || key === "Воскресенье") && key === today:
-                    document.writeln(`<p><i><b>${key}</b></i></p>`);
-                    break;
-               case key === "Суббота" || key === "Воскресенье":
-                    document.writeln(`<p><i>${key}</i></p>`);
-                    break;
-               case key === today:
-                    document.writeln(`<p><b>${key}</b></p>`);
-                    break;
-               default:
-                    document.writeln(`<p>${key}</p>`);
-                    break;
-          }
-     }
-};
-weekList(week);
+week.forEach((day, index) => {
+  let str = day;
+    
+  today === index && (str = `<b>${str}</b>`);
+  index > 4 && (str = `<i>${str}</i>`);
+    
+  document.body.insertAdjacentHTML('beforeend', `<p>${str}</p>`);
+}); 

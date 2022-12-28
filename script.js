6dd40f8751ab88b18696123d65513e4c49320076
Viacheslav Kuminov <kuminov200220@gmail.com>
2022-12-28 19:12:33 +0700

    19_1

--- script.js
+++ script.js
@@ -1,50 +1,15 @@
 "use strict";
 
-let today;
-let hr;
-let min;
-let sec;
+const userInput = document.getElementById("user-input");
+const userOutput = document.getElementById("user-output");
+let intervalID;
 
-let options = {
-  weekday: 'long',
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric'
+const showText = () => {
+  userOutput.textContent = userInput.value;
+  console.log("Функция работает");
 };
 
-function getNewDay() {
-    today = new Date();
-    hr = today.getHours();
-    min = today.getMinutes();
-    sec = today.getSeconds();
-};
-
-function numstr(n, text_forms) {
-    let m = Math.abs(n) % 100;
-    let n1 = m % 10;
-    if (m > 10 && m < 20) {
-        return text_forms[2];
-    }
-    if (n1 > 1 && n1 < 5) {
-        return text_forms[1];
-    }
-    if (n1 === 1) {
-        return text_forms[0];
-  }
-  
-    return text_forms[2];
-};
-
-function addZero(num) {
-    if (num.toString().length === 1) num = "0" + num;
-    return num;
-};
-
-const timer = function () {
-  getNewDay();
-  today = `Сегодня ${today.toLocaleDateString('ru-RU', options)} ${hr} ${numstr(hr, [' час ', ' часа ', ' часов '])} ${min} ${numstr(min, [' минута ', ' минуты ', ' минут '])} ${sec} ${numstr(sec, [' секунда ', ' секунды ', ' секунд '])} <br/> ${today.toLocaleDateString('ru-RU')} - ${addZero(hr)}:${addZero(min)}:${addZero(sec)}`;
-  document.body.innerHTML = today;
-};
-
-timer();
-setInterval(timer, 1000);
+userInput.addEventListener("input", () => {
+  clearTimeout(intervalID);
+  intervalID = setTimeout(showText, 300);
+});

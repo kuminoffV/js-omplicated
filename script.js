25c943b8e7c7d55fe688fd9dc2cbc9220b5ed526
Viacheslav Kuminov <kuminov200220@gmail.com>
2022-12-28 19:41:16 +0700

    19_2

--- script.js
+++ script.js
@@ -1,15 +1,62 @@
 "use strict";
 
-const userInput = document.getElementById("user-input");
-const userOutput = document.getElementById("user-output");
-let intervalID;
+const timer = () => {
+  const text = document.getElementById("text");
+  const newDate = () => {
+    let date = new Date();
+    let today = date.toLocaleDateString("ru-Ru", {
+      weekday: "long",
+    });
+    let dayTime;
+    let timeNow = date.toLocaleTimeString("en");
+    let dateStop = new Date(date.getFullYear() + 1, 0, 1);
+    let timeRemaining = dateStop.getTime() - date.getTime();
+    let dayRemining = Math.floor(timeRemaining / 1000 / 60 / 60 / 24);
+    let titleDayRemining = "12";
 
-const showText = () => {
-  userOutput.textContent = userInput.value;
-  console.log("Функция работает");
+    switch (true) {
+      case date.getHours() < 5: {
+        dayTime = "Доброй ночи";
+        break;
+      }
+
+      case date.getHours() < 12: {
+        dayTime = "Доброе утро";
+        break;
+      }
+
+      case date.getHours() < 18: {
+        dayTime = "Добрый день";
+        break;
+      }
+
+      case date.getHours() <= 24: {
+        dayTime = "Добрый вечер";
+        break;
+      }
+    }
+
+    return {
+      dayTime,
+      today,
+      timeNow,
+      dayRemining,
+    };
+  };
+
+  const showTime = () => {
+    let time = newDate();
+    text.innerHTML = `${time.dayTime} <br />
+        Сегодня: ${time.today} <br />
+        Текущее время: ${time.timeNow} <br />
+        До нового года осталось ${time.dayRemining} дня`;
+  };
+
+  showTime();
+
+  setInterval(() => {
+    showTime();
+  }, 1000);
 };
 
-userInput.addEventListener("input", () => {
-  clearTimeout(intervalID);
-  intervalID = setTimeout(showText, 300);
-});
+timer();

a0f4d2e586855c5204b9a797b4b5202dc7288c52
Viacheslav Kuminov <kuminov200220@gmail.com>
2022-12-28 20:13:11 +0700

    19_3

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
 "use strict";
 
-const userInput = document.getElementById("user-input");
-const userOutput = document.getElementById("user-output");
-let intervalID;
+const man = document.getElementById("man");
+const air = document.getElementById("air");
+const btnFly = document.getElementById("animation");
+const btnDump = document.getElementById("animation_1");
+const btnReset = document.getElementById("reset");
+let countFly = 0;
+let countDump = 0;
+let activeFly = false;
+let activeDump = false;
+let flyTime;
+let flyDumpTime;
 
-const showText = () => {
-  userOutput.textContent = userInput.value;
-  console.log("Функция работает");
+const flyAir = () => {
+  countFly++;
+  flyTime = requestAnimationFrame(flyAir);
+  countFly < 400
+    ? (air.style.left = countFly * 4 + "px")
+    : cancelAnimationFrame(flyTime);
 };
 
-userInput.addEventListener("input", () => {
-  clearTimeout(intervalID);
-  intervalID = setTimeout(showText, 300);
+btnFly.addEventListener("click", () => {
+  if (activeFly) {
+    cancelAnimationFrame(flyTime);
+    activeFly = !activeFly;
+  } else {
+    flyTime = requestAnimationFrame(flyAir);
+    activeFly = !activeFly;
+  }
+});
+
+const flyDump = () => {
+  countDump++;
+  flyDumpTime = requestAnimationFrame(flyDump);
+  countDump < 450
+    ? (man.style.top = countDump * 2 + "px")
+    : cancelAnimationFrame(flyDumpTime);
+};
+
+btnDump.addEventListener("click", () => {
+  if (activeDump) {
+    cancelAnimationFrame(flyDumpTime);
+    activeDump = !activeDump;
+  } else {
+    flyDumpTime = requestAnimationFrame(flyDump);
+    activeDump = !activeDump;
+  }
+});
+
+btnReset.addEventListener("click", () => {
+  cancelAnimationFrame(flyTime);
+  cancelAnimationFrame(flyDumpTime);
+  countFly = 0;
+  countDump = 0;
+  activeFly = false;
+  activeDump = false;
+  air.style.left = 0;
+  man.style.top = 0;
 });

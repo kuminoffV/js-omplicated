d1c8399f32051bfd006cae8bbe0377b369ac1e08
Viacheslav Kuminov <kuminov200220@gmail.com>
2023-01-04 01:32:31 +0700

    19_1

--- script.js
+++ script.js
@@ -2,18 +2,20 @@
 
 const userInput = document.getElementById("user-input");
 const userOutput = document.getElementById("user-output");
-let intervalID;
+// let intervalID;
 
 const showText = () => {
   userOutput.textContent = userInput.value;
   console.log("Функция работает");
 };
 
-const intervalFunk = () => {
-  clearTimeout(intervalID);
-  intervalID = setTimeout(showText, 300);
-};
+// const intervalFunk = () => {
+//   clearTimeout(intervalID);
+//   intervalID = setTimeout(showText, 300);
+// };
 
 userInput.addEventListener("input", () => {
-  intervalFunk();
+  let intervalID;
+  clearTimeout(intervalID);
+  intervalID = setTimeout(showText, 300);
 });
